974cb50543c632c43c297fc1f324c9d2d0a9cdbf
xingzhou <xingzhou@cn.ibm.com>
2014-03-19 17:32:06 +0800

    headless #12

--- spec/integration/web/admin_spec.rb
+++ spec/integration/web/admin_spec.rb
@@ -49,9 +49,12 @@ describe AdminUI::Admin, :type => :integration, :firefox_available => true do
     context 'tabs' do
       before do
 
-        Selenium::WebDriver::Wait.new(:timeout => 10).until { puts '*******' + @driver.find_element(:class_name => 'menuItemSelected').attribute('id') + '/' + tab_id;@driver.find_element(:id => tab_id).click;@driver.find_element(:class_name => 'menuItemSelected').attribute('id') == tab_id }
+        Selenium::WebDriver::Wait.new(:timeout => 5).until {
+          @driver.find_element(:id => tab_id).click
+          @driver.find_element(:class_name => 'menuItemSelected').attribute('id') == tab_id
+        }
         # Enforce that the page is displayed
-        Selenium::WebDriver::Wait.new(:timeout => 10).until { @driver.find_element(:id => "#{ tab_id }Page").displayed? }
+        Selenium::WebDriver::Wait.new(:timeout => 5).until { @driver.find_element(:id => "#{ tab_id }Page").displayed? }
       end
 
       context 'Organizations' do
