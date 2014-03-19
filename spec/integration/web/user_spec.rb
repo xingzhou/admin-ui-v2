44fd12ef01089448e76220ab78823c21f71ab830
xingzhou <xingzhou@cn.ibm.com>
2014-03-19 16:54:24 +0800

    headless #9

--- spec/integration/web/user_spec.rb
+++ spec/integration/web/user_spec.rb
@@ -12,17 +12,26 @@ describe AdminUI::Admin, :type => :integration, :firefox_available => true do
     end
 
     it 'does not have a create DEA button' do
-      @driver.find_element(:id => 'DEAs').click
-      expect(@driver.find_element(:class_name => 'menuItemSelected').attribute('id')).to eq('DEAs')
-      expect(@driver.find_element(:id => 'DEAsPage').displayed?).to be_true
-      expect(@driver.find_element(:id => 'ToolTables_DEAsTable_0').text).to eq('Copy')
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'DEAs').click
+        @driver.find_element(:class_name => 'menuItemSelected').attribute('id') == 'DEAs'
+      }
+
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'DEAsPage').displayed? &&
+        @driver.find_element(:id => 'ToolTables_DEAsTable_0').text == 'Copy'
+      }
     end
 
     it 'does not have a remove all components button' do
-      @driver.find_element(:id => 'Components').click
-      expect(@driver.find_element(:class_name => 'menuItemSelected').attribute('id')).to eq('Components')
-      expect(@driver.find_element(:id => 'ComponentsPage').displayed?).to be_true
-      expect(@driver.find_element(:id => 'ToolTables_ComponentsTable_0').text).to eq('Copy')
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'Components').click
+        @driver.find_element(:class_name => 'menuItemSelected').attribute('id') == 'Components'
+      }
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'ComponentsPage').displayed? &&
+        @driver.find_element(:id => 'ToolTables_ComponentsTable_0').text == 'Copy'
+      }
     end
 
     it 'does not have a tasks tab' do
@@ -30,10 +39,14 @@ describe AdminUI::Admin, :type => :integration, :firefox_available => true do
     end
 
     it 'does not have a create stats button' do
-      @driver.find_element(:id => 'Stats').click
-      expect(@driver.find_element(:class_name => 'menuItemSelected').attribute('id')).to eq('Stats')
-      expect(@driver.find_element(:id => 'StatsPage').displayed?).to be_true
-      expect(@driver.find_element(:id => 'ToolTables_StatsTable_0').text).to eq('Copy')
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'Stats').click
+        @driver.find_element(:class_name => 'menuItemSelected').attribute('id') == 'Stats'
+      }
+      Selenium::WebDriver::Wait.new(:timeout => 5).until {
+        @driver.find_element(:id => 'StatsPage').displayed? &&
+        @driver.find_element(:id => 'ToolTables_StatsTable_0').text == 'Copy'
+      }
     end
   end
 end
